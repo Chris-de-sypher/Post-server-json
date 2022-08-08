666934421ca0ce15f43573ab9add2bcbccdfa65a
Chris-de-sypher <christiansilvester700@gmail.com>
2022-08-08 19:40:04 +0100

    up to date

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create(); // create json-server
+const router = jsonServer.router('post.json')  // put in the file 
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 8300; // default port or 8300
+
+
+server.use(middleware); // the defaults
+server.use(router); // the file created
+
+
+server.listen(port) // listen to the port
